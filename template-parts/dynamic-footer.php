a654847b968fe6f214cc8d3934e903f6da1aa8dc
Alabaich <enjoyable.design@gmail.com>
2024-08-01 12:04:39 -0400

    footer 2

--- template-parts/dynamic-footer.php
+++ template-parts/dynamic-footer.php
@@ -21,6 +21,11 @@ $footer_nav_menu = wp_nav_menu( [
 ] );
 ?>
 <footer class="pageWidth">
+	<span class="goToTop">
+	<svg xmlns="http://www.w3.org/2000/svg" width="23" height="12" viewBox="0 0 23 12" fill="none">
+  		<path d="M0.5 11.1502C0.5 11.3241 0.573847 11.5022 0.717198 11.6357C1.0039 11.9027 1.47305 11.9027 1.75975 11.6357L11.5728 2.49666L21.2424 11.5022C21.5291 11.7692 21.9983 11.7692 22.285 11.5022C22.5717 11.2352 22.5717 10.7982 22.285 10.5312L12.094 1.03619C11.8073 0.769185 11.3382 0.769185 11.0515 1.03619L0.717198 10.6607C0.569503 10.7982 0.5 10.9722 0.5 11.1502Z" fill="black"/>
+	</svg>
+	</span>
 	<div class="innerFooter">
 		<div class="newsletter">
 			<p>
@@ -53,6 +58,15 @@ $footer_nav_menu = wp_nav_menu( [
 		</div>
 	</div>
 </footer>
+
+<script>
+document.querySelector('.goToTop').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+</script>
 <!-- <footer id="site-footer" class="site-footer dynamic-footer <?php echo esc_attr( $footer_class ); ?>">
 	<div class="footer-inner">
 		<div class="site-branding show-<?php echo esc_attr( hello_elementor_get_setting( 'hello_footer_logo_type' ) ); ?>">
